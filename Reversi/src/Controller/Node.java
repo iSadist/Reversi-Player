eb0f7cd9bda76bb6e27029ce903c1f42d25bcac0
Jan Svensson <jsvensson51@yahoo.com>
2017-02-05 01:26:00 +0100

    Implement Node
    Implemented but not complete because it is not decided how it should work yet.

--- Reversi/src/Controller/Node.java
+++ Reversi/src/Controller/Node.java
@@ -6,9 +6,15 @@ public class Node {
 	
 	private int myValue;
 	private int opponentsScore;
+	private int value;
+	private boolean valueIsSet;
+	private boolean myTurn;
+	private Point nextMove;
 	private ArrayList<Node> branches;
 	
-	public Node(int value) {
+	public Node(int myScore, int computerScore) {
+		value = 0;
+		valueIsSet = false;
 		branches = new ArrayList<Node>();
 	}
 	
@@ -16,7 +22,33 @@ public class Node {
 		branches.add(node);
 	}
 	
+	public Node getNode(int place) {
+		return branches.get(place);
+	}
 	
+	public int children() {
+		return branches.size();
+	}
 	
+	public ArrayList<Node> getChildren() {
+		return branches;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+		valueIsSet = true;
+	}
+	
+	public boolean hasValue() {
+		return valueIsSet;
+	}
+	
+	public Point getMove() {
+		return nextMove;
+	}
 	
 }
