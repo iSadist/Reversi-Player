0123c6dca4fc8244338b2c47170fb4e2de055411
Jan Svensson <jsvensson51@yahoo.com>
2017-02-05 01:22:38 +0100

    Implement depth-first limited method
    This method does NOT contain alpha/beta yet! It looks through the whole tree
    and finds the best node and sends it back. Note that these 3 methods are
    dependent on each other.

--- Reversi/src/Controller/TreeBuilder.java
+++ Reversi/src/Controller/TreeBuilder.java
@@ -1,5 +1,68 @@
 package Controller;
 
 public class TreeBuilder {
-
+	
+	private Node root;
+	
+	public TreeBuilder(Node root) {
+		this.root = root;
+	}
+	
+	public void setRootTo(Node node) {
+		this.root = node;
+	}
+	
+	public void buildTreeWithDepth(int depth) {
+		
+	}
+	
+	/**
+	 * Returns the next Node with the highest value in the tree for a given depth
+	 */
+	public Node depthFirstLimited(int depth) {
+		Node bestNode = null;
+		int highestValue = Integer.MIN_VALUE;
+		int currentNodeValue;
+		
+		for(Node childNode : root.getChildren()) {
+			currentNodeValue = getMinValue(childNode, 0, depth);
+			if(currentNodeValue > highestValue) {
+				highestValue = currentNodeValue;
+				bestNode = childNode;
+			}
+		}
+		return bestNode;
+	}
+	
+	
+	private int getMinValue(Node root, int currentDepth, int depth) {
+		if(currentDepth >= depth || root.children() == 0) return root.getValue();
+		
+		int lowestValue = Integer.MAX_VALUE;
+		int currentNodeValue;
+		
+		for(Node childNode : root.getChildren()) {
+			currentNodeValue = getMaxValue(childNode, 0, depth);
+			if(currentNodeValue < lowestValue) {
+				lowestValue = currentNodeValue;
+			}
+		}
+		return lowestValue;
+	}
+	
+	private int getMaxValue(Node root, int currentDepth, int depth) {
+		if(currentDepth >= depth || root.children() == 0) return root.getValue();
+		
+		int highestValue = Integer.MIN_VALUE;
+		int currentNodeValue;
+		
+		for(Node childNode : root.getChildren()) {
+			currentNodeValue = getMinValue(childNode, 0, depth);
+			if(currentNodeValue < highestValue) {
+				highestValue = currentNodeValue;
+			}
+		}
+		return highestValue;
+	}
+	
 }
